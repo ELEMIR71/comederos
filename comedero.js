d3538155e188a9a18f4da9bf044837418358634d
Emir <124746082+ELEMIR71@users.noreply.github.com>
2023-08-05 15:52:24 -0300

    Update comedero.js

--- comedero.js
+++ comedero.js
@@ -5,21 +5,6 @@ var segundosLabel = document.getElementById('segundos');
 var segundos = 0;
 var intervalId = null;
 
-connectBtn.addEventListener('click', function() {
-  var redWifiInput = document.getElementById('redWifi');
-  var contrasenaInput = document.getElementById('contrasena');
-  var redWifi = redWifiInput.value;
-  var contrasena = contrasenaInput.value;
-
-  // Aquí puedes realizar la lógica para conectar el ESP8266 a la red Wi-Fi utilizando los valores ingresados
-  // Esto puede implicar el uso de bibliotecas o métodos específicos del ESP8266
-
-  // Ejemplo de conexión utilizando la biblioteca WiFi.h
-  // WiFi.begin(redWifi, contrasena);
-
-  // Resto del código para la conexión y configuración del ESP8266
-});
-
 encendidoBtn.addEventListener('click', function() {
   encendidoBtn.classList.add('active');
   apagadoBtn.classList.remove('active');
